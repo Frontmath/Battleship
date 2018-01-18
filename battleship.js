053c8b283888f3fed42152a6e058c9b864e1fb3c
Frontmath <serg.bereznevatiy@gmail.com>
2018-01-19 00:26:16 +0200

    first commit

---
+++ battleship.js
@@ -0,0 +1,209 @@
+var view = {
+	displayMessage: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
+	},
+	displayMiss: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
+	}
+};
+
+// // ************* TEST ****************
+
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
+
+// view.displayMessage("Tap tap, is this thing on?");
+
+
+
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipsSunk: 0,
+	shipLength: 3,
+	ships: [ { locations: [0, 0, 0], hits: ["", "", ""] },
+			 { locations: [0, 0, 0], hits: ["", "", ""] },
+			 { locations: [0, 0, 0], hits: ["", "", ""] } ],
+	generateShipLocations: function() {
+		var locations;
+		for (var i = 0; i < this.numShips; i++) {
+			do {
+				locations = this.generateShip();
+			} while (this.collision(locations));
+			this.ships[i].locations = locations;
+		}
+	},
+	generateShip: function() {
+		var direction = Math.floor(Math.random() * 2);
+		var row, col;
+		if (direction === 1) {
+			row = Math.floor(Math.random() * this.boardSize);
+			col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+		} else {
+			row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+			col = Math.floor(Math.random() * this.boardSize);
+		}
+
+		var newShipLocations = [];
+		for (var i = 0; i < this.shipLength; i++) {
+			if (direction === 1) {
+				newShipLocations.push(row + "" + (col + i)); // Круглые скобки гарантируют, что значение i будет 
+				// прибавлено к col до преобразования результата в строку.
+			} else {
+				newShipLocations.push((row + i) + "" + col);
+			}
+		}
+		return newShipLocations;
+	},
+	collision: function(locations) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = model.ships[i];
+			for (var j = 0; j < locations.length; j++) {
+				if (ship.locations.indexOf(locations[j]) >= 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	},
+	fire: function(guess) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			// var locations = ship.locations;
+			// var index = locations.indexOf(guess);
+			var index = ship.locations.indexOf(guess); // используем "сцепление", чтобы сократить код до 1 строки!
+			if (index >= 0) {
+				ship.hits[index] = "hit";
+				view.displayHit(guess); // Оповещаем представление о том, что в клетке guess следует вывести маркер попадания.
+				view.displayMessage("HIT!"); // И приказываем представлению вывести сообщение “HIT!”.
+				if (this.isSunk(ship)) {
+					view.displayMessage("You sank my battleship!"); // Сообщаем игроку, что он потопил корабль!
+					this.shipsSunk++;
+				}
+				return true;
+			}
+		}
+		view.displayMiss(guess); // Сообщаем представлению, что в клетке guess следует вывести маркер промаха.
+		view.displayMessage("You missed."); // И приказываем представлению вывести сообщение о промахе.
+		// Методы объекта представления добавляют класс “hit” или “miss” к элементу с идентификатором, содержащимся
+		// в guess. Таким образом, представление преобразует «попадания» из массива hits в разметку HTML. Не забывайте,
+		// что HTML-«попадания» нужны для отображения информации, а «попадания» в модели представляют фактическое
+		// состояние.
+		return false;
+	},
+	isSunk: function(ship) {
+		for (var i = 0; i < this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+// ************************ Тест-драйв fire
+
+// model.fire("53");
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
+// model.fire("34");
+// model.fire("24");
+// model.fire("44");
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
+
+var controller = {
+	guesses: 0,
+	processGuess: function(guess) {
+		var location = parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips) {
+				view.displayMessage("You sank all my battleships,<br> in " +
+				this.guesses + " guesses!");
+			}
+		}
+	}
+};
+
+// ******************* ТЕСТ-ДРАЙВ controller
+
+// controller.processGuess("A0");
+// controller.processGuess("A6");
+// controller.processGuess("B6");
+// controller.processGuess("C6");
+// controller.processGuess("C4");
+// controller.processGuess("D4");
+// controller.processGuess("E4");
+// controller.processGuess("B0");
+// controller.processGuess("B1");
+// controller.processGuess("B2");
+
+function parseGuess(guess) {
+	var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+	if (guess === null || guess.length !== 2) {
+		alert("Oops, please enter a letter and a number on the board.");
+	} else {
+		firstChar = guess.charAt(0);
+		var row = alphabet.indexOf(firstChar);
+		var column = guess.charAt(1);
+		if (isNaN(row) || isNaN(column)) {
+			alert("Oops, that isn't on the board.");
+		} else if (row < 0 || row >= model.boardSize ||
+			column < 0 || column >= model.boardSize) {
+			alert("Oops, that's off the board!");
+		} else {
+			return row + column; // В этой точке все проверки пройдены, поэтому метод может вернуть результат.
+		}
+	}
+	return null; // Если управление передано в эту точку, значит, какая-то проверка не прошла, и метод возвращает null.
+};
+
+// ******************* ТЕСТ-ДРАЙВ parseGuess
+
+// console.log(parseGuess("A0"));
+// console.log(parseGuess("B6"));
+// console.log(parseGuess("G3"));
+// console.log(parseGuess("H0"));
+// console.log(parseGuess("A7"));
+
+function init() {
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+	var guessInput = document.getElementById("guessInput");
+	guessInput.onkeypress = handleKeyPress;
+
+	model.generateShipLocations(); // вызывается из функции init, чтобы это происходило во время загрузки игры
+	// (до ее начала). При таком вызове позиции всех кораблей будут определены к моменту начала игры.
+};
+
+function handleFireButton() {
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value; // Координаты хранятся в свойстве value элемента input.
+
+	controller.processGuess(guess); // Координаты выстрела передаются контроллеру
+	guessInput.value = "";  
+};
+
+function handleKeyPress(e) {
+	var fireButton = document.getElementById("fireButton");
+	if (e.keyCode === 13) {
+		fireButton.click();
+		return false;
+	}
+};
+
+window.onload = init;
